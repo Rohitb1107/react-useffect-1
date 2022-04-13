25ac9e7c69f02c0dd913274ce99da3fc417c838b
Rohit <stylishrb711@gmail.com>
2022-04-13 22:54:51 +0530

    useEffect done

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,52 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      const data = await fetch("http://localhost:8000/users").then((res) =>
-        res.json()
-      );
-      setUsers(data);
-    }
-    getData();
-  }, []);
-
+  const [counter, setCounter] = useState(0);
+  
   return (
     <>
-      <div>
-        {users.map((el) => (
-          <div>
-            {el.id}.{el.first_name}
-          </div>
-        ))}
-      </div>
+      <h2>Counter: {counter}</h2>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Add 1
+      </button>
     </>
   );
 }
 
 export default App;
+
+// import { useEffect, useState } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     async function getData() {
+//       const data = await fetch("http://localhost:8000/users").then((res) =>
+//         res.json()
+//       );
+//       setUsers(data);
+//     }
+//     getData();
+//   }, []);
+
+//   return (
+//     <>
+//       <div>
+//         {users.map((el) => (
+//           <div>
+//             {el.id}.{el.first_name}
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
